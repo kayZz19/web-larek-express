2e1d5f25566ac8bfee83a7ea8d40fed554b71bda
Roman Kirillov <roman.kirillov.06@mail.ru>
2026-05-18 13:10:40 +0300

    firstTry/backed

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use(requestLogger);
 app.use("/", productRoutes);
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, "../../public")));
 
 app.use("*", (req, res, next) => {
   next(new NotFoundError("Маршрут не найден"));
